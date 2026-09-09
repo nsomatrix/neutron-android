3ed0d1a63f9d1faf88c54b85e5701a0c8185aa28
corestorage <corestorage@yahoo.com>
2026-09-09 20:03:23 +0530

    gitignore: ignore build directories and remove build artifacts

--- app/build/intermediates/cxx/Debug/3o2m3h4g/obj/local/x86_64/objs-debug/javam3g/m3g/CSynchronization.o.d
+++
@@ -1,5 +0,0 @@
-/home/manish/projects/neutron-android/app/build/intermediates/cxx/Debug/3o2m3h4g/obj/local/x86_64/objs-debug/javam3g/m3g/CSynchronization.o: \
-  /home/manish/projects/neutron-android/app/src/main/cpp/m3g/CSynchronization.cpp \
-  /home/manish/projects/neutron-android/app/src/main/cpp/m3g/inc/CSynchronization.hpp
-
-/home/manish/projects/neutron-android/app/src/main/cpp/m3g/inc/CSynchronization.hpp:
