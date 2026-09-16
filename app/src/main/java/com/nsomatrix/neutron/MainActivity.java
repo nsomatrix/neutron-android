29a591ecf8569c446580c99f4b0cf5fcec670324
corestorage <corestorage@yahoo.com>
2026-09-16 09:11:14 +0530

    removed-warning

--- app/src/main/java/com/nsomatrix/neutron/MainActivity.java
+++ app/src/main/java/com/nsomatrix/neutron/MainActivity.java
@@ -47,7 +47,6 @@ import com.nsomatrix.neutron.util.PickDirResultContract;
 import com.nsomatrix.neutron.installer.InstallerDialog;
 
 import static com.nsomatrix.neutron.util.Constants.PREF_EMULATOR_DIR;
-import static com.nsomatrix.neutron.util.Constants.PREF_STORAGE_WARNING_SHOWN;
 import static com.nsomatrix.neutron.util.Constants.PREF_TOOLBAR;
 
 public class MainActivity extends BaseActivity {
@@ -85,11 +84,6 @@ public class MainActivity extends BaseActivity {
 		if (!preferences.contains(PREF_TOOLBAR)) {
 			boolean enable = !ViewConfiguration.get(this).hasPermanentMenuKey();
 			preferences.edit().putBoolean(PREF_TOOLBAR, enable).apply();
-		}
-		boolean warningShown = preferences.getBoolean(PREF_STORAGE_WARNING_SHOWN, false);
-		if (!FileUtils.isExternalStorageLegacy() && !warningShown) {
-			showScopedStorageDialog();
-			preferences.edit().putBoolean(PREF_STORAGE_WARNING_SHOWN, true).apply();
 		}
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 	}
@@ -136,16 +130,6 @@ public class MainActivity extends BaseActivity {
 			Toast.makeText(this, R.string.permission_request_failed, Toast.LENGTH_SHORT).show();
 			finish();
 		}
-	}
-
-	private void showScopedStorageDialog() {
-		String message = getString(R.string.scoped_storage_warning) + Config.getEmulatorDir();
-		new AlertDialog.Builder(this)
-				.setTitle(R.string.warning)
-				.setCancelable(false)
-				.setMessage(message)
-				.setPositiveButton(android.R.string.ok, null)
-				.show();
 	}
 
 	private void onPickDirResult(Uri uri) {
